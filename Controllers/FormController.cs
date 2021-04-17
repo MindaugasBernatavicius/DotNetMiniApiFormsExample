e61938bbe77515c165203b1dce42ff4540b799ce
Mindaugas <darbas.mindaugas@gmail.com>
2021-04-17 15:48:54 +0300

    Pushing all of it into git

---
+++ Controllers/FormController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetMiniBe.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class FormController : ControllerBase
+    {
+        private readonly ILogger<FormController> _logger;
+
+        public FormController(ILogger<FormController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("form")]
+        public ActionResult Index()
+        {
+            string form = "<html>" 
+                + "<head><title>Form demo</title></head>" 
+                + "<body>"
+                + "<form action=\"\" method=\"POST\">"
+                + "<label for=\"firstname\">Firstname:</label><br>"
+                + "<input type=\"text\" name=\"firstname\"><br>"
+                + "<label for=\"lastname\">Lastname:</label><br>"
+                + "<input type=\"text\" name=\"lastname\"><br>"
+                + "<input type=\"submit\" value=\"Send\">"
+                + "</form>"
+                + "</body>" 
+                + "</html>";
+            return Content(form, "text/html");
+        }
+
+        [HttpPost]
+        [Route("form")]
+        public string JsonStringBody()
+        {
+            string resp;
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var body = reader.ReadToEndAsync();
+                Console.WriteLine(body.Result);
+                resp = body.Result;
+            }
+
+            return resp;
+        }
+    }
+}
---
+++ Controllers/RoutesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetMiniBe.Controllers
+{
+    [Route("routes")]
+    public class RoutesController : Controller
+    {
+        private readonly IActionDescriptorCollectionProvider _provider;
+
+        public RoutesController(IActionDescriptorCollectionProvider provider)
+        {
+            _provider = provider;
+            foreach (var item in _provider.ActionDescriptors.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetRoutes()
+        {
+            var routes = _provider.ActionDescriptors.Items.Select(x => new
+            {
+                Action = x.RouteValues["Action"],
+                Controller = x.RouteValues["Controller"],
+                //Name = x.AttributeRouteInfo.Name,
+                //Template = x.AttributeRouteInfo.Template
+            }).ToList();
+            return Ok(routes);
+        }
+    }
+}
